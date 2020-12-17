172822e0138b0af18c4af403a3ef233ffc05499b
bambooxv <bambooxv@gmail.com>
2020-12-17 09:34:16 -0500

    Added a nav bar and many other things

--- Script.js
+++ Script.js
@@ -1,1 +1,36 @@
 // JavaScript Document
+$( document ).ready(function() {
+$("#content1").hide();
+$("#content2").hide();
+$("#content3").hide();
+$("#content4").hide();
+
+$("#button1").click(function(){
+$("#content1").show();
+$("#content2").hide();
+$("#content3").hide();
+$("#content4").hide();
+});
+	
+$("#button2").click(function(){
+$("#content1").hide();
+$("#content2").show();
+$("#content3").hide();
+$("#content4").hide();
+});
+	
+$("#button3").click(function(){
+$("#content1").hide();
+$("#content2").hide();
+$("#content3").show();
+$("#content4").hide();
+});
+	
+$("#button4").click(function(){
+$("#content1").hide();
+$("#content2").hide();
+$("#content3").hide();
+$("#content4").show();
+});
+	
+});
